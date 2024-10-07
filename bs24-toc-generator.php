db8433600202746b00647e03e2dce04e658bd351
md-hiron <hironmd647@gmail.com>
2024-10-07 12:12:17 +0600

    ignored generateBlock heading

--- bs24-toc-generator.php
+++ bs24-toc-generator.php
@@ -17,7 +17,7 @@ define('BS24_TOS_URL', plugin_dir_url(__FILE__));
 /**
  * Making translateable
  */
-add_action( 'plugins_loaded', array( $this, 'bs24_load_textdomain' ) );
+add_action( 'plugins_loaded', 'bs24_load_textdomain' );
 
 function bs24_load_textdomain(){
     load_plugin_textdomain( 'bs24_tos', false, BS24_TOS_URL . 'languages/' );
@@ -62,7 +62,7 @@ function bs24_toc_shortcode( $atts ) {
  */
 function bs24_create_toc( $content ) {
     // Use regex to find all <h2> and <h3> tags
-    preg_match_all('/<h2.*?>(.*?)<\/h2>|<h3.*?>(.*?)<\/h3>/', $content, $matches, PREG_SET_ORDER);
+    preg_match_all('/<h2(?![^>]*class="[^"]*gb-headline[^"]*").*?>(.*?)<\/h2>|<h3(?![^>]*class="[^"]*gb-headline[^"]*").*?>(.*?)<\/h3>/', $content, $matches, PREG_SET_ORDER);
 
     if (empty($matches)) {
         return ''; // No headings found, no TOC needed.
@@ -111,8 +111,7 @@ function bs24_add_anchors( $content ) {
     if (!is_singular(['post', 'page'])) {
         return $content; // If not the specified post types, return the content unmodified
     }
-
-    preg_match_all('/<h2.*?>(.*?)<\/h2>|<h3.*?>(.*?)<\/h3>|<div.*?class="(.*?)gb-headline(.*?)".*?>.*?<h2.*?>(.*?)<\/h2>|<div.*?class="(.*?)gb-headline(.*?)".*?>.*?<h3.*?>(.*?)<\/h3>/', $content, $matches, PREG_SET_ORDER);
+    preg_match_all('/<h2(?![^>]*class="[^"]*gb-headline[^"]*").*?>(.*?)<\/h2>|<h3(?![^>]*class="[^"]*gb-headline[^"]*").*?>(.*?)<\/h3>/', $content, $matches, PREG_SET_ORDER);
 
     if ( empty( $matches ) ) {
         return $content; // No headings found, no TOC needed.
@@ -122,21 +121,20 @@ function bs24_add_anchors( $content ) {
     $h3_count = 0;
 
     foreach ($matches as $match) {
-        if (!empty($match[1]) || !empty($match[5]) ) {
+        if (!empty($match[1])) {
             // It's an <h2>
             $h2_count++;
             $h3_count = 0; // Reset h3 count for each new h2
-            $heading_text = strip_tags(!empty($match[1]) ? $match[1] : $match[5]);
+            $heading_text = strip_tags($match[1]);
             $heading_anchor = preg_replace('/[^a-zA-Z0-9]/', '-', sanitize_title( $heading_text ) );
-            $content = preg_replace('/<h2.*?>' . preg_quote($match[1], '/') . '<\/h2>/', '<h2 id="' . esc_attr( $heading_anchor ) . '">' . esc_html( $match[1] ) . '</h2>', $content, 1);
-            $content = preg_replace('/<div.*?class="(.*?)gb-headline(.*?)".*?>.*?<h2.*?>' . preg_quote($heading_text, '/') . '<\/h2>/', '<div class="$1gb-headline$2"><h2 id="' . esc_attr($heading_anchor) . '">' . esc_html($heading_text) . '</h2>', $content, 1);
-        } elseif ( !empty($match[2]) || !empty( $match[8] ) ) {
+            $content = preg_replace('/<h2(?![^>]*class="[^"]*gb-headline[^"]*").*?>' . preg_quote($match[1], '/') . '<\/h2>/', '<h2 id="' . esc_attr( $heading_anchor ) . '">' . wp_kses_post($match[1]) . '</h2>', $content, 1);
+        } elseif ( !empty($match[2]) ) {
             // It's an <h3>
             $h3_count++;
-            $heading_text = strip_tags(!empty( $match[2] ) ? $match[2] : $match[8]);
+            $heading_text = strip_tags($match[2]);
             $heading_anchor = preg_replace('/[^a-zA-Z0-9]/', '-', sanitize_title( $heading_text ) );
-            $content = preg_replace('/<h3.*?>' . preg_quote($match[2], '/') . '<\/h3>/', '<h3 id="' . $heading_anchor . '">' . $match[2] . '</h3>', $content, 1);
-            $content = preg_replace('/<div.*?class="(.*?)gb-headline(.*?)".*?>.*?<h3.*?>' . preg_quote($heading_text, '/') . '<\/h3>/', '<div class="$1gb-headline$2"><h3 id="' . esc_attr($heading_anchor) . '">' . esc_html($heading_text) . '</h3>', $content, 1);
+            $content = preg_replace('/<h3(?![^>]*class="[^"]*gb-headline[^"]*").*?>' . preg_quote($match[2], '/') . '<\/h3>/', '<h3 id="' . esc_attr( $heading_anchor ) . '">' . wp_kses_post($match[2]) . '</h3>', $content, 1);
+      
         }
     }
 
